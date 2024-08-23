f89167a0019fbe75449e81dafbe333ab512c041a
valed-dm <dmvaled@gmail.com>
2024-08-23 08:47:43 +0300

    teleplaywright.py got user search protection and comments

--- teleplaywright.py
+++ teleplaywright.py
@@ -6,17 +6,29 @@ group_name = 'test'
 throttle_delay = 0
 
 
-async def invite_user_to_channel(page, username):
+async def invite_user_to_channel(page, username, g_name):
+    # selector for '<div>' where found user resides
+    div_selector = 'div.ListItem.chat-item-clickable.picker-list-item.has-ripple'
+
     try:
+        # Group panel is opened to press 'Add user' button
         await page.click('div.MiddleHeader')
         await page.click('button[title="Add users"]')
+        # username inserted into search input
         await page.fill('input#new-members-picker-search', username)
+        # waiting for search result to hit the target user exactly
         await asyncio.sleep(1)
         # await page.screenshot(path="images/user_add_user.png")
 
-        await page.click('div.ListItem.chat-item-clickable.picker-list-item.has-ripple')
+        # erroneous user search result protection
+        div_element = await page.query_selector(div_selector)
+        if div_element is None:
+            raise Exception(f"User '{username}' not added to group '{g_name}'.")
+        # user is checked to be added to group
+        await div_element.click()
         # await page.screenshot(path="images/user_add_ready.png")
 
+        # Finally we add user to group
         await page.click('button[title="Add users"]')
         print(f"Successfully invited {username}")
 
@@ -26,7 +38,7 @@ async def invite_user_to_channel(page, username):
 
 async def main():
     async with async_playwright() as p:
-        # Create a persistent context
+        # Create a persistent context to pass Telegram auth limits
         context = await p.chromium.launch_persistent_context(
             user_data_dir="session",
             headless=False
@@ -48,13 +60,15 @@ async def main():
         await page.click(f'text={group_name}', force=True)
         # await page.screenshot(path="images/user_group_search_1.png")
 
+        # Target Group is found
         parent_div = page.locator('div.search-section')
         await parent_div.get_by_role('button', name=group_name, exact=True).click(force=True)
         # await page.screenshot(path="images/user_group_search_2.png")
 
         # Iterate over the user list and invite them to the group/channel
         for username in user_list:
-            await invite_user_to_channel(page, username)
+            await invite_user_to_channel(page, username, g_name=group_name)
+            # For testing purpose to follow Telegram restrictions
             await asyncio.sleep(throttle_delay)
 
         await context.close()
